2756c688ef07f2dcf1c5a73e12bf46bf7cf18fb8
ashpil <shpilenok2001@gmail.com>
2021-01-16 13:32:05 +0300

    removes routify starter mess

--- src/pages/_fallback.svelte
+++ src/pages/_fallback.svelte
@@ -7,11 +7,8 @@
     font-size: 12rem;
   }
   .e404 {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
     text-align: center;
+    margin: 20rem 0;
   }
 </style>
 
@@ -22,3 +19,4 @@
   <a href={$url('../')}>Go back</a>
   </div>
 </div>
+
