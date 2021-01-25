c8b7452ff05cf06fd2e54b2d6f6b50003b2cc080
ashpil <shpilenok2001@gmail.com>
2021-01-25 05:08:40 +0300

    adds data collecting

--- api/server.ts
+++ api/server.ts
@@ -2,11 +2,38 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import child_process from 'child_process';
 import fs from 'fs';
+import pg from 'pg';
+import formidable from 'express-formidable';
+
 const app = express();
 
 const TEMPLATE = 'dist/__app.html';
 const PORT = 5000;
 const GATOR_DIR = `${__dirname}/../dist/stdlib`;
+
+const pool = new pg.Pool({
+  user: 'postgres',
+  database: 'gator-study',
+  port: 5432,
+})
+
+// On start up we just add the table if it doesn't exist yet
+pool.query(`CREATE TABLE IF NOT EXISTS data(
+  userid VARCHAR(22),
+  image bytea,
+  code text,
+  time timestamp
+);`).catch(e => console.log(e));
+
+// Uncomment this to be able to see data in your browser at `/data`
+// app.get('/data', async (req, res) => {
+//   pool.query('SELECT * FROM data ORDER BY userid ASC', (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     res.status(200).json(results.rows)
+//   })
+// })
 
 // helper function to compile gator
 // this could be vastly simplified/improved with some changes to the gator executable
@@ -24,11 +51,23 @@ function gatorc(content: string): Promise<string> {
   })
 }
 
-app.use(bodyParser.text());
+// puts info into database
+app.use('/update', formidable());
+app.put('/update', async (req, res) => {
+  const { userid, code } = (req as any).fields;
+  const image = fs.readFileSync((req as any).files.image.path);
 
-// serve gator compilation POST
-app.post('/compile', async (req, res) => {
-  // have it send 202 on error so we can tell on client
+  pool
+    .query('INSERT INTO data VALUES ($1, $2, $3, current_timestamp)', [userid, image, code])
+    .catch(e => console.error(e.stack))
+
+  res.end();
+})
+
+// serve gator compilation PUT
+app.use('/compile', bodyParser.text());
+app.put('/compile', async (req, res) => {
+  // have it send 202 on error so we can tell client something went wrong
   gatorc(req.body).then(glsl => res.send(glsl), e => res.status(202).send(e));
 })
 
